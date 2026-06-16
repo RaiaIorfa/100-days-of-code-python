6f640ebf05126af43ff8be989516e97352afef94
Raphael Atile <iorfaatile@gmail.com>
2026-06-16 19:24:41 +0100

    Implement ball movement, wall collision and bouncing logic in Pong game

--- day22_PongGame/ball.py
+++ day22_PongGame/ball.py
@@ -1,5 +1,53 @@
 from turtle import Turtle
+import random
 
 class Ball(Turtle):
-    def __init__(self, shape = "square", undobuffersize = 1000, visible = True):
-        super().__init__(shape, undobuffersize, visible)
+    MOVE_DISTANCE = 10
+    def __init__(self):
+        super().__init__(shape ="square")
+        self.color("white")
+        self.penup()
+        # self.direction = self.heading()
+        
+    def move(self, direction = ""):
+        if direction == "top_left":
+            new_x = self.xcor() + self.MOVE_DISTANCE
+            new_y = self.ycor() - self.MOVE_DISTANCE
+        elif direction == "top_right":
+            new_x = self.xcor() - self.MOVE_DISTANCE
+            new_y = self.ycor() - self.MOVE_DISTANCE
+        elif direction == "bottom_right":
+            new_x = self.xcor() + self.MOVE_DISTANCE
+            new_y = self.ycor() + self.MOVE_DISTANCE
+        elif direction == "bottom_left":
+            new_x = self.xcor() - self.MOVE_DISTANCE
+            new_y = self.ycor() + self.MOVE_DISTANCE
+        else:
+            new_x = self.xcor() + self.MOVE_DISTANCE
+            new_y = self.ycor() + self.MOVE_DISTANCE
+
+        self.goto(new_x, new_y)
+
+        # if self.direction >= 0 and self.direction < 90:
+        #     self.goto(self.xcor() + self.MOVE_DISTANCE, self.ycor() + self.MOVE_DISTANCE)
+        # elif self.direction >= 90 and self.direction < 180:
+        #     self.goto(self.xcor() - self.MOVE_DISTANCE, self.ycor() + self.MOVE_DISTANCE)
+        # elif self.direction >= 180 and self.direction < 270:
+        #     self.goto(self.xcor() - self.MOVE_DISTANCE, self.ycor() - self.MOVE_DISTANCE)
+        # elif self.direction >= 270:
+        #     self.goto(self.xcor() + self.MOVE_DISTANCE, self.ycor() - self.MOVE_DISTANCE)
+
+    def bounce(self, boundary):
+        if boundary == "top":
+            if self.xcor() > 300 - self.xcor():
+                return "top_left"
+            else:
+                return "top_right"
+        elif boundary == "bottom":
+            if self.xcor() > 300 - self.xcor():
+                return "bottom_left"
+            else:
+                return "bottom_right"
+
+
+    
--- day22_PongGame/main.py
+++ day22_PongGame/main.py
@@ -1,11 +1,15 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.tracer(0)
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("Pong")
+
+ball = Ball()
 p1 = Paddle((-350,0))
 p2 = Paddle((350,0))
 
@@ -16,7 +20,15 @@ screen.onkey(p1.move_up, "w")
 screen.onkey(p1.move_down, "s")
 
 game_is_on = True
+direction = ""
 while game_is_on:
+    time.sleep(0.2)
     screen.update()
+    ball.move(direction)
+    if ball.ycor() > 280:
+        direction = ball.bounce("top")
+    elif ball.ycor() < -280:
+        direction = ball.bounce("bottom")
+
 
 screen.exitonclick()
