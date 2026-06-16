bbdad4457108647694c17e4ee5613a328b05f015
Raphael Atile <iorfaatile@gmail.com>
2026-06-16 21:30:17 +0100

    Refactor ball collision logic and remove unused code in Pong game

--- day22_PongGame/ball.py
+++ day22_PongGame/ball.py
@@ -1,8 +1,6 @@
 from turtle import Turtle
-import random
 
 class Ball(Turtle):
-    # MOVE_DISTANCE = 10
     def __init__(self):
         super().__init__(shape ="square")
         self.color("white")
@@ -13,45 +11,16 @@ class Ball(Turtle):
     def move(self):
         new_x = self.xcor() + self.x_move
         new_y = self.ycor() + self.y_move
-        # print(f"Moving To: \n new Y-cooridinate: {new_y} | new X-coordinate: {new_x}")
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_y(self):
         self.y_move *= -1
-        # print(f"Y-cooridinate: {self.ycor()} | X-coordinate: {self.xcor()}")
-        # print("_" * 100)
 
-
-    # def move(self, direction = ""):
-    #     if direction == "top_left":
-    #         new_x = self.xcor() + self.MOVE_DISTANCE
-    #         new_y = self.ycor() - self.MOVE_DISTANCE
-    #     elif direction == "top_right":
-    #         new_x = self.xcor() - self.MOVE_DISTANCE
-    #         new_y = self.ycor() - self.MOVE_DISTANCE
-    #     elif direction == "bottom_right":
-    #         new_x = self.xcor() + self.MOVE_DISTANCE
-    #         new_y = self.ycor() + self.MOVE_DISTANCE
-    #     elif direction == "bottom_left":
-    #         new_x = self.xcor() - self.MOVE_DISTANCE
-    #         new_y = self.ycor() + self.MOVE_DISTANCE
-    #     else:
-    #         new_x = self.xcor() + self.MOVE_DISTANCE
-    #         new_y = self.ycor() + self.MOVE_DISTANCE
-
-    #     self.goto(new_x, new_y)
-
-    # def bounce(self, boundary):
-    #     if boundary == "top":
-    #         if self.xcor() > 300 - self.xcor():
-    #             return "top_left"
-    #         else:
-    #             return "top_right"
-    #     elif boundary == "bottom":
-    #         if self.xcor() > 300 - self.xcor():
-    #             return "bottom_left"
-    #         else:
-    #             return "bottom_right"
+    def bounce_x(self):
+        self.x_move *= -1
+    
+    def reset_position(self):
+        self.goto(0,0)
 
 
     
--- day22_PongGame/main.py
+++ day22_PongGame/main.py
@@ -20,19 +20,34 @@ screen.onkey(p1.move_up, "w")
 screen.onkey(p1.move_down, "s")
 
 game_is_on = True
-direction = ""
 while game_is_on:
     time.sleep(0.2)
     screen.update()
-    ball.move()
-    if ball.ycor() > 280 or ball.ycor() < -280:
-        ball.bounce()
 
-    # ball.move(direction)
-    # if ball.ycor() > 280:
-    #     ball.bounce("top")
-    # elif ball.ycor() < -280:
-    #     ball.bounce("bottom")
+    ball.move()
+    
+    # Detect ball wall collision 
+    if ball.ycor() > 280 or ball.ycor() <= -280:
+        ball.bounce_y()
+    
+    # Detect collision with paddle/pong_stick
+    # Detect collision with Right Paddle (p2)
+    if ball.distance(p2) < 50 and ball.xcor() > 320:
+        ball.bounce_x()
+
+    # Detect collision with Left Paddle (p1)
+    elif ball.distance(p1) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    
+    # Detect ball out of bounds Right Side(p2)
+    if ball.xcor() > 380:
+        ball.reset_position()
+        ball.bounce_x()
+
+    # Detect ball out of bounds Left Side(p1)
+    elif ball.xcor() < -380:
+        ball.reset_position()
+        ball.bounce_x()
 
 
 screen.exitonclick()
