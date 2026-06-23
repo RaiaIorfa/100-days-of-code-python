ea41bc51adc4ecf62ab0bb6bb3af3b2e8329b1ce
Raphael Atile <iorfaatile@gmail.com>
2026-06-23 05:54:10 +0100

    Refactor weather data analysis and add squirrel census data processing
    - Updated import statement for pandas to use alias 'pd'.
    - Removed commented-out code related to weather data processing.
    - Added functionality to read and analyze squirrel census data.
    - Created a DataFrame for fur color counts and saved it to 'squirrel_count.csv'.
    - Included analysis for adult squirrels in the dataset.

--- Day25/main.py
+++ Day25/main.py
@@ -15,17 +15,39 @@
 #             pass
 #     print(temperatures)
 
-import pandas
+import pandas as pd
 import os
 
-script_dir = os.path.dirname(__file__)  # folder where main.py is located
-file_path = os.path.join(script_dir, "./weather_data.csv")
+# script_dir = os.path.dirname(__file__)  # folder where main.py is located
+# file_path = os.path.join(script_dir, "./weather_data.csv")
 
-data = pandas.read_csv(file_path)
-temperature = data["temp"]
-print(data)
-print(temperature)
+# data = pandas.read_csv(file_path)
+# # temperature = data["temp"]
+# # print(data)
+# # print(temperature)
 
 
-print(temperature.mean())
-print(temperature.max())
+# # print(temperature.mean())
+# # print(temperature.max())
+# # print(data[temperature == temperature.max()])
+# monday = data[data.day == "Monday"]
+# # print(monday)
+# monday_temp = (monday.temp * 1.8) + 32
+# print(f"{monday_temp} F")
+
+df = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+data = df["Primary Fur Color"].value_counts()
+
+fur_color = pd.DataFrame(data)
+print(fur_color)
+fur_color.rename(columns={'Primary Fur Color': 'Fur Color', 'count': 'Count'})
+print(fur_color)
+fur_color.to_csv("squirrel_count.csv")
+
+adult_squirrels = df[df["Age"] == "Adult"]
+print(adult_squirrels.value_counts())
+
+# Fur Color, Count
+# grey, 2473
+# red, 392
+# black, 103
